4757d98d209ef181442033e1f055755bc5c0bcf2
abdelrhman39 <abdelrhmanatwa39@gmail.com>
2021-09-07 01:39:52 +0300

    first commit

---
+++ project/js/main.js
@@ -0,0 +1,47 @@
+/* Strat Div Modal */
+var modal1 = document.getElementById("startPyment");
+var modal2 = document.getElementById("paymentMethod");
+var modal3 = document.getElementById("finshPaymentMethod");
+var modal4 = document.getElementById("resultPaymentMethod");
+/* End Div Modal */
+
+/* Strat Btn Modal */
+
+var btn_modal1 = document.getElementById("modal1");
+var btn_modal2 = document.getElementById("modal2");
+var btn_modal3 = document.getElementById("modal3");
+var btn_modal4 = document.getElementById("modal4");
+/* End Btn Modal */
+
+/* Start Function Modals */
+
+btn_modal1.onclick = function () {
+    'use strict';
+    modal1.style.display = 'none';
+};
+
+
+btn_modal2.onclick = function () {
+    'use strict';
+    modal1.style.display = 'none';
+    modal2.style.display = 'none';
+};
+btn_modal3.onclick = function () {
+    'use strict';
+    modal1.style.display = 'none';
+    modal2.style.display = 'none';
+    modal3.style.display = 'none';
+};
+
+
+/* End Function Modals */
+
+
+
+
+
+
+
+
+
+
